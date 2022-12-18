5254a38083f9b748464f70dcdf78be398d2676fd
Pestiz <arkadiuszczachura98@gmail.com>
2022-12-18 19:11:07 +0100

    first portfolio

---
+++ js/portfolio.js
@@ -0,0 +1,40 @@
+console.log ('chuj');
+
+let menu = document.getElementById('menu-bars');
+let header = document.querySelector('header');
+
+let cursor1 = document.querySelector('.cursor-1');
+let cursor2 = document.querySelector('.cursor-2');
+
+document.querySelectorAll('a').forEach(links =>{
+   
+    links.onmouseenter = () =>{
+        cursor1.classList.add('active');
+        cursor2.classList.add('active');
+    }
+
+    links.onmouseleave = () =>{
+        cursor1.classList.remove('active');
+        cursor2.classList.remove('active');
+    }
+
+})
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+
+window.onmousemove = (e) => {
+    cursor1.style.top = e.pageY + 'px';
+    cursor1.style.left = e.pageX + 'px';
+    cursor2.style.top = e.pageY + 'px';
+    cursor2.style.left = e.pageX + 'px';
+}
+
